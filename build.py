3100a735bc3a904f4f4bef73e3aa2a436740c43d
Michel Meyer <meyer_michel@outlook.com>
2020-12-15 21:54:45 +0100

    build.py: add  raise exception

--- build.py
+++ build.py
@@ -1,3 +1,13 @@
 import os
-os.system('npm install')
-os.system('vsce package')
+
+def run(cmd, assert_error=False):
+    print("*********** Running: %s" % cmd)
+    ret = os.system(cmd)
+    if ret == 0 and assert_error:
+        raise Exception("Command unexpectedly succedeed: %s" % cmd)
+    if ret != 0 and not assert_error:
+        raise Exception("Failed command: %s" % cmd)
+
+if __name__ == "__main__":
+    run('npm install')
+    run('vsce package')
