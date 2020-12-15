e5f65629ec18bcb68bf4a828156a7ebac7eafbd6
Michel Meyer <meyer_michel@outlook.com>
2020-12-15 22:42:13 +0100

    add exit code on HOST

--- createWithDocker.py
+++ createWithDocker.py
@@ -1,5 +1,17 @@
-import os
+import os, sys, traceback
+
+def run(cmd, assert_error=False):
+    print("*********** HOST Running: %s" % cmd)
+    ret = os.system(cmd)
+    if ret == 0 and assert_error:
+        raise Exception("Command unexpectedly succedeed: %s" % cmd)
+    if ret != 0 and not assert_error:
+        raise Exception("Failed command: %s" % cmd)
 
 if __name__ == "__main__":
-    os.system("docker build --rm . -t vscodeext_build")
-    os.system("docker run  --rm --workdir /app -v %s:/app vscodeext_build:latest" % os.getcwd())
+    try:
+        run('docker build --rm . -t vscodeext_build')
+        run(f'docker run  --rm --workdir /app -v {os.getcwd()}:/app vscodeext_build:latest')
+    except:
+        traceback.print_exc()
+        sys.exit(1)
