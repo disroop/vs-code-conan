5899f0ee9831b70194d4c6f3b72addba78c78512
Carlos A. Wong <carloswong54@gmail.com>
2021-06-09 21:20:55 -0400

    Updated general-settings class profile & workspace directories, user name, and channel

--- src/configurator/general-settings.ts
+++ src/configurator/general-settings.ts
@@ -2,20 +2,62 @@ import * as vscode from 'vscode';
 
 export class GeneralSettings {
     private conanPath: string;
-    private profilesDirectory: string;
-    
+    private profilesDir: string;
+    private workspacesDir: string;
+    private user: string;
+    private channel: string;
 
     constructor(conanPath: string = "conan",
-                profilesDirectory: string = "") {
+                profilesDir: string = "",
+                workspacesDir: string = "",
+                user: string = "default",
+                channel: string = "development") {
         this.conanPath = conanPath;
-        this.profilesDirectory = profilesDirectory;
+        this.profilesDir = profilesDir;
+        this.workspacesDir = workspacesDir;
+        this.user = user;
+        this.channel = channel;
     }
 
     getConanPath(): string {
         return this.conanPath;
     }
-    
-    setConanPath(conanPath: string) {
-        this.conanPath = conanPath;
+
+    hasProfilesDir(): boolean {
+        return this.profilesDir.length > 0 ? true : false;
+    }
+
+    getProfilesDir(): string {
+        return this.profilesDir;
+    }
+
+    getWorkspacesDir(): string {
+        return this.workspacesDir;
+    }
+
+    getUser(): string {
+        return this.user;
+    }
+
+    getChannel(): string {
+        return this.channel;
+    }
+
+    getJson(): object {
+        let profilesDir = this.profilesDir;
+        let workspacesDir = this.workspacesDir;
+        const rootPath = vscode.workspace.workspaceFolders?.[0];
+        if (rootPath) {
+            const rootFsPath : string = rootPath.uri.fsPath;
+            profilesDir   = this.profilesDir.replace(rootFsPath,"${workspaceFolder}");
+            workspacesDir = this.workspacesDir.replace(rootFsPath,"${workspaceFolder}");
+        }
+        return {
+            conanPath: this.conanPath,
+            profilesDir: profilesDir,
+            workspacesDir: workspacesDir,
+            user: this.user,
+            channel: this.channel
+        };
     }
 }
