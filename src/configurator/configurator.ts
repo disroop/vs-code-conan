f3dac7c230a7dd9e2954c5768364e5c502ab7aa1
Carlos A. Wong <carloswong54@gmail.com>
2021-06-09 22:23:03 -0400

    Configurator class has a process to setup and generate a user's conan-settingss.json file

--- src/configurator/configurator.ts
+++ src/configurator/configurator.ts
@@ -1,19 +1,55 @@
 /* eslint-disable eqeqeq */
+import * as vscode from 'vscode';
 import { SettingsParser } from "./settings-parser";
 import { Profile } from "./profile";
 import { Workspace } from "./workspace";
 import { GeneralSettings } from "./general-settings";
+import { output } from "../extension";
 
 interface ProfileExtended {
     build: string | undefined;
     host: string | undefined;
 }
 
+const conanPathDialogOptions: vscode.OpenDialogOptions = {
+    canSelectFolders: false,
+    canSelectFiles: true,
+    title: "Select conan path:"
+};
+const profilesDirDialogOptions: vscode.OpenDialogOptions = {
+    canSelectFolders: true,
+    canSelectFiles: false,
+    title: "Select profiles directory:"
+};
+const workspacesDirDialogOptions: vscode.OpenDialogOptions = {
+    canSelectFolders: true,
+    canSelectFiles: false,
+    title: "Select workspaces directory:"
+};
+const userInputBoxOpt: vscode.InputBoxOptions = {
+    prompt: "Enter conan user name.",
+    value: "default"
+};
+const channelInputBoxOpt: vscode.InputBoxOptions = {
+    prompt: "Enter conan channel.",
+    value: "development"
+};
+const setupProfilesAndWorkspacesDirsOpt: vscode.QuickPickOptions = {
+    canPickMany:false,
+    placeHolder:"Both"
+};
+
 export class Configurator {
     private readonly file: string;
     private settings: GeneralSettings = new GeneralSettings();
     private profiles: Map<string, Profile> = new Map<string, Profile>();
     private workspaces: Map<string, Workspace> = new Map<string, Workspace>();
+    private user: string = "";
+    private channel: string = "";
+    private conanPath: string = "conan";
+    private profilesDir: string = "";
+    private workspacesDir: string  = "";
+    private tryAgainCounter: number = 0;
 
     constructor(file: string) {
         this.file = file;
@@ -31,6 +67,10 @@ export class Configurator {
         return this.settings.getConanPath();
     }
 
+    getProfilesDir(): string {
+        return this.settings.getProfilesDir();
+    }
+
     getConanFile(name: string): string {
         let conanFile = this.isWorkspace(name)
             ? this.workspaces.get(name)?.getConanWorkspace()
@@ -41,6 +81,16 @@ export class Configurator {
         return conanFile;
     }
 
+    getProfileOrWorkspace(name:string): Profile|Workspace {
+        let out = this.isWorkspace(name)
+            ? this.workspaces.get(name)
+            : this.profiles.get(name);
+        if (!out) {
+            throw new Error("No existing profile or workspace found.");
+        }
+        return out;
+    }
+
     getAllNames(): string[] {
         const profileNames = Array.from(this.profiles.keys());
         const workspaceNames = Array.from(this.workspaces.keys());
@@ -49,6 +99,14 @@ export class Configurator {
             throw new Error("Duplication of names");
         }
         return names;
+    }
+
+    getAllEnabledNames(): string[] {
+        return this.getAllNames().filter( (name) => {
+            if (this.getProfileOrWorkspace(name).isEnabled()) {
+                return name;
+            }
+        });
     }
 
     checkUniqueName(names: string[]): boolean {
@@ -143,5 +201,146 @@ export class Configurator {
             throw new Error("No createChannel found");
         }
         return createChannel;
+    }
+
+    initSetupFile() {
+        vscode.window.showInformationMessage(
+            "Would you like to begin Disroop Conan setup? Or setup manually?",
+            "Yes", "No", "Manual").then( value => {
+                if (!value) { return; }
+                else if (value === "Yes") {
+                    this.setupConfigs().then( () => {
+                        this.settings = new GeneralSettings(
+                            this.conanPath,
+                            this.profilesDir,
+                            this.workspacesDir,
+                            this.user,
+                            this.channel);
+                        this.generateConanSettingsFile();
+                    }, reason => {
+                        console.error(reason);
+                    });
+                } else if (value === "Manual") {
+                    this.generateDefaultConanSettingsFile();
+                }
+            }
+        );
+    }
+
+    async setupConfigs(): Promise<void | undefined> {
+        try {
+            await vscode.window.showInformationMessage(
+                "Use conan from path environment variable?", "Yes", "No")
+                .then( value => {
+                    if (!value) { return; }
+                    else if (value === "No") {
+                        vscode.window.showOpenDialog(conanPathDialogOptions)
+                            .then( value => {
+                                if (!value) { return; }
+                                this.conanPath = value[0].fsPath;
+                        });
+                    }
+                });
+            await vscode.window.showInputBox(userInputBoxOpt)
+                .then( value => {
+                    if (!value) { return; }
+                    this.user = value;
+                    output.appendLine(`Username: ${this.user}`);
+                });
+            await vscode.window.showInputBox(channelInputBoxOpt)
+                .then( value => {
+                    if (!value) { return; }
+                    this.channel = value;
+                    output.appendLine(`Channel: ${this.channel}`);
+                });
+            let option :string = "";
+            const options_list :string[] = ["Both","Profiles","Workspaces"];
+            await vscode.window.showQuickPick(options_list, setupProfilesAndWorkspacesDirsOpt)
+                .then( value => {
+                    if (!value) { return; }
+                    option = value;
+                })
+                .then( () => {
+                    if ( option.length === 0 ) {
+                        throw new Error(`A selection needs to be made from ${options_list}`);
+                    }
+                });
+            await this.setupProfilesAndWorkspacesDirs(option);
+        } catch {
+            return this.trySetupAgain();
+        }
+    }
+
+    async setupProfilesAndWorkspacesDirs(option: string): Promise< void | undefined > {
+        output.appendLine(`Option: ${option}`);
+        console.log(`Option: ${option}`);
+        if (option === "Profiles" || option === "Both") {
+            await vscode.window.showOpenDialog(profilesDirDialogOptions)
+            .then( value => {
+                if (!value) { return; }
+                this.profilesDir = value[0].fsPath;
+                output.appendLine(`Profiles directory: ${this.profilesDir}`);
+                console.log(`Profiles directory: ${this.profilesDir}`);
+                this.profiles = SettingsParser.profilesFromDir(this.profilesDir);
+            });
+        }
+        if (option === "Workspaces" || option === "Both") {
+            await vscode.window.showOpenDialog(workspacesDirDialogOptions)
+            .then( value => {
+                if (!value) { return; }
+                this.workspacesDir = value[0].fsPath;
+                output.appendLine(`Workspaces directory: ${this.workspacesDir}`);
+                this.workspaces = SettingsParser.workspacesFromDir(this.workspacesDir);
+            });
+        }
+    }
+
+    async trySetupAgain(): Promise<void | undefined> {
+        await vscode.window.showInformationMessage(
+            "Setup cancelled. Would you like to try again or manually setup Disroop Conan?",
+            "Try again", "Manual setup")
+            .then( value => {
+                output.appendLine(`Try again opt: ${value}`);
+                if (this.tryAgainCounter<=2) {
+                    if (value === "Try again") { 
+                        this.tryAgainCounter++;
+                        this.initSetupFile();
+                    } else {
+                        this.generateDefaultConanSettingsFile();
+                    }
+                } else {
+                    throw new Error("Disroop Conan: Setup of conan-settings.json unsuccessful!");
+                }
+            });
+    }
+
+    generateDefaultConanSettingsFile() {
+        const out = { 
+            settings:   new GeneralSettings().getJson(),
+            profiles:   [new Profile().getJson()],
+            workspaces: [new Workspace().getJson()]
+        };
+        this.writeConanJsonFile(out);
+    }
+
+    generateConanSettingsFile() {
+        const out = {
+            settings:   this.settings.getJson(),
+            profiles:   Array.from(this.profiles.values()).map(
+                        function (x) { return x.getJson(); }),
+            workspaces: Array.from(this.workspaces.values()).map(
+                        function (x) { return x.getJson(); })
+        };
+        this.writeConanJsonFile(out);
+    }
+
+    writeConanJsonFile(out:object) {
+        const fs = require("fs");
+        const outJson = JSON.stringify(out, null, 4);
+        fs.writeFile(this.file, outJson, function (err:any) {
+            if (err) {
+                throw err;
+            }
+        });
     }
 }
