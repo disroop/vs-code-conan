4ac382703cd52fe3516e33655d9ca8771c322d71
Michel Meyer <meyer_michel@outlook.com>
2021-10-04 20:27:06 +0200

    move suit to integration-test

--- test/integration-test/suite/profile.test.ts
+++ test/integration-test/suite/profile.test.ts
@@ -3,7 +3,7 @@ import * as assert from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-import {Profile} from '../../configurator/profile';
+import {Profile} from '../../../src/configurator/profile';
 
 suite('Extension Test Suite', () => {
     vscode.window.showInformationMessage('Start all tests.');
